49c0b5330971b0afcc63dc80ca49a7bd56d74b5f
pptichka <taleskina@gmail.com>
2022-10-02 19:24:34 +0300

    clean the code

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -22,11 +22,6 @@ class LoginViewController: UIViewController {
     
     // MARK: Methods
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let greetingVC = segue.destination as? WelcomeViewController else { return }
         greetingVC.userName = userNameTF.text
@@ -40,7 +35,11 @@ class LoginViewController: UIViewController {
     }
     
     private func showAlert() {
-        let alert = UIAlertController(title: "Login Error!", message: "Username or Password not found.", preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: "Login Error!",
+            message: "Username or Password not found.",
+            preferredStyle: .alert
+        )
         alert.addAction(UIAlertAction(title: "Try again", style: .cancel))
         
         present(alert, animated: true)
@@ -52,7 +51,6 @@ class LoginViewController: UIViewController {
             passwordTF.text = emptyText
         }
     }
-   
     
     // MARK: IBActions
     
@@ -62,14 +60,22 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func userNameShowHelp() {
-        let alert = UIAlertController(title: "Help", message: "Username is \(correctUserName)", preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: "Help",
+            message: "Username is \(correctUserName)",
+            preferredStyle: .alert
+        )
         alert.addAction(UIAlertAction(title: "OK", style: .cancel))
         
         present(alert, animated: true)
     }
     
     @IBAction func passwordShowHelp() {
-        let alert = UIAlertController(title: "Help", message: "Password is \(correctPassword).", preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: "Help",
+            message: "Password is \(correctPassword).",
+            preferredStyle: .alert
+        )
         alert.addAction(UIAlertAction(title: "OK", style: .cancel))
         
         present(alert, animated: true)
