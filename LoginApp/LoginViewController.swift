145efde89debebca4569e6e914446fd055b9bd3f
pptichka <taleskina@gmail.com>
2022-10-02 19:15:47 +0300

    create alerts

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,18 +9,29 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: IBOutlets
     
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
+    
+    // MARK: Data
+    
+    let correctUserName = "user"
+    let correctPassword = "password"
+    let emptyText = ""
+    
+    // MARK: Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let greetingVC = segue.destination as? WelcomeViewController else { return }
         greetingVC.userName = userNameTF.text
+        
+        showAlert()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -28,10 +39,34 @@ class LoginViewController: UIViewController {
         self.view.endEditing(true)
     }
     
-    @IBAction func unwindToStart(_ sender: UIStoryboardSegue) {
-        userNameTF.text = ""
-        passwordTF.text = ""
+    private func showAlert() {
+        let alert = UIAlertController(title: "Login Error!", message: "Username or Password not found.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Try again", style: .cancel))
+        
+        present(alert, animated: true)
     }
-
+   
+    
+    // MARK: IBActions
+    
+    @IBAction func unwindToStart(_ sender: UIStoryboardSegue) {
+        userNameTF.text = emptyText
+        passwordTF.text = emptyText
+    }
+    
+    @IBAction func userNameShowHelp() {
+        let alert = UIAlertController(title: "Help", message: "Username is \(correctUserName)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        
+        present(alert, animated: true)
+    }
+    
+    @IBAction func passwordShowHelp() {
+        let alert = UIAlertController(title: "Help", message: "Password is \(correctPassword).", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        
+        present(alert, animated: true)
+    }
+    
 }
 
