d9f47432d4338a300321e9259578db50827c52c4
pptichka <taleskina@gmail.com>
2022-10-05 20:43:42 +0300

    all are correct!

--- LoginApp/ViewControllers/BioViewController.swift
+++ LoginApp/ViewControllers/BioViewController.swift
@@ -14,12 +14,28 @@ class BioViewController: UIViewController {
     
     private let user = User(information: .getPerson())
     
+    private let firstColor = UIColor(
+        red: 230.0 / 255.0,
+        green: 159.0 / 255.0,
+        blue: 223.0 / 255.0,
+        alpha: 1.0
+    )
+    
+    private let secondColor = UIColor(
+        red: 173.0 / 255.0,
+        green: 239.0 / 235.0,
+        blue: 223.0 / 255.0,
+        alpha: 1.0
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         firstBioLabel.text = user.information.mainInfo
         secondBioLabel.text = user.information.workInfo
         thirdBioLabel.text = user.information.dreamInfo
+        
+        view.addVerticalGradientLayer(topColor: firstColor, buttomColor: secondColor)
     }
 
 }
