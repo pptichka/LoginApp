9a588cc83445e01cbf3127e99d7aa2891cfe186a
pptichka <taleskina@gmail.com>
2022-10-05 15:18:34 +0300

    placed new views in VC

---
+++ LoginApp/ViewControllers/BioViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BioViewController.swift
+//  LoginApp
+//
+//  Created by Ксн Тлскн on 05.10.2022.
+//
+
+import UIKit
+
+class BioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
